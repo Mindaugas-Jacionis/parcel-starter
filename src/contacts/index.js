6e7777b6f816863ca379e72218ab972f983dfd04
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2019-06-06 20:06:29 +0300

    Adds edit, search, delete functionality & UI

--- src/contacts/index.js
+++ src/contacts/index.js
@@ -1,16 +1,27 @@
 let contacts = [];
 
-const getContacts = () => contacts;
+const getContacts = () => [...contacts];
 const addContact = contact => {
-  contacts = [...contacts, { ...contact, id: new Date().getTime() }];
+  contacts = [...contacts, { ...contact, id: new Date().getTime(), editing: false }];
 };
 
 const deleteContact = id => {
-  contacts = contacts.filter(contact => contact.id == id);
+  contacts = contacts.filter(contact => contact.id != id);
+};
+
+const updateContact = (id, newInfo) => {
+  contacts = contacts.map(contact => {
+    if (contact.id == id) {
+      return { ...contact, ...newInfo };
+    }
+
+    return contact;
+  });
 };
 
 module.exports = {
   getContacts,
   addContact,
   deleteContact,
+  updateContact,
 };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,61 @@
 const contacts = require('./contacts');
+require('./index.scss');
+
+let searchValue = '';
 
 function render() {
   const addressBook = contacts.getContacts();
   const app = document.getElementById('app');
+  app.innerHTML = null;
 
-  addressBook.forEach(contact => {
-    const container = document.createElement('div');
-    const name = document.createElement('span');
-    const phone = document.createElement('span');
+  addressBook
+    .filter(contact => {
+      const normalizedName = contact.name.toLowerCase();
+      const normalizedSearch = searchValue.toLowerCase();
 
-    name.textContent = contact.name;
-    phone.textContent = contact.phone;
+      const hasName = normalizedName.includes(normalizedSearch);
+      const hasPhone = contact.phone.includes(normalizedSearch);
 
-    container.appendChild(name);
-    container.appendChild(phone);
-    app.appendChild(container);
-  });
+      return hasName || hasPhone;
+    })
+    .forEach(contact => {
+      const container = document.createElement('div');
+      const name = document.createElement(contact.editing ? 'input' : 'span');
+      const phone = document.createElement(contact.editing ? 'input' : 'span');
+      const deleteButton = document.createElement('button');
+      const editButton = document.createElement('button');
+
+      name[contact.editing ? 'value' : 'textContent'] = contact.name;
+      phone[contact.editing ? 'value' : 'textContent'] = contact.phone;
+      deleteButton.textContent = '❌';
+      deleteButton.type = 'button';
+      editButton.textContent = contact.editing ? '✅' : '✏️';
+      editButton.type = 'button';
+
+      deleteButton.addEventListener('click', () => {
+        contacts.deleteContact(contact.id);
+        render();
+      });
+
+      phone.addEventListener('input', e => {
+        contacts.updateContact(contact.id, { phone: e.target.value });
+      });
+
+      name.addEventListener('input', e => {
+        contacts.updateContact(contact.id, { name: e.target.value });
+      });
+
+      editButton.addEventListener('click', () => {
+        contacts.updateContact(contact.id, { editing: !contact.editing });
+        render();
+      });
+
+      container.appendChild(name);
+      container.appendChild(phone);
+      container.appendChild(deleteButton);
+      container.appendChild(editButton);
+      app.appendChild(container);
+    });
 }
 
 function addFormListener(e) {
@@ -28,9 +68,16 @@ function addFormListener(e) {
   render();
 }
 
+function searchFormListener(e) {
+  searchValue = e.target.value;
+  render();
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('add-form');
+  const search = document.getElementById('search-form');
 
   form.addEventListener('submit', addFormListener);
+  search.addEventListener('input', searchFormListener);
   render();
 });
