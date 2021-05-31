fc7b1ff1a1f3c33974a10295942f6226eea73e84
Mindaugas Jačionis <m.jacionis@gmail.com>
2021-05-31 12:22:56 +0300

    Add tailwind config sample

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/index.html", "./src/**/*.{js,html}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
